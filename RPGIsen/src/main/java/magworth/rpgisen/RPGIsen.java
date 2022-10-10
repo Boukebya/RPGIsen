9ffcdc87e5aa3cdbff878b9a77db7bc6d52a6ff2
Yanis.Boukebir <62990521+Boukebya@users.noreply.github.com>
2022-10-10 17:05:24 +0200

    Test commit

--- RPGIsen/src/main/java/magworth/rpgisen/RPGIsen.java
+++ RPGIsen/src/main/java/magworth/rpgisen/RPGIsen.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package magworth.rpgisen;
 
 /**
@@ -13,5 +8,7 @@ public class RPGIsen {
 
     public static void main(String[] args) {
         System.out.println("Hello World feur!");
+        System.out.println("feur feur");
+        System.out.println("Hihi");
     }
 }
