da6818340a565881f527963cb871a42f3fa56603
Clovis-A <72743019+Clovis-A@users.noreply.github.com>
2022-10-24 11:01:51 +0200

    test Intellij

--- src/Class/wildLife/Character.java
+++ src/Class/wildLife/Character.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package wildLife;
+package Class.wildLife;
 
 /**
  *
@@ -11,13 +11,13 @@ package wildLife;
 public class Character extends Entity{
     int experience;
     
-    public Character(String name,int strenght){
-        super(name, strenght);
+    public Character(String name,int strength){
+        super(name, strength);
     }
     
     public void getExperience(int xp){
         /*
-        Manage the XP of the character an his level
+        Manage the XP of the character and his level
         */
         this.experience+=xp;
         if(this.experience>=10){
--- src/Class/wildLife/Enemie.java
+++ src/Class/wildLife/Enemie.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package wildLife;
+package Class.wildLife;
 
 import aroundLife.Equipement;
 
@@ -13,8 +13,8 @@ import aroundLife.Equipement;
 public class Enemie extends Entity{
     int dropChance;// on a range 0 to 10    
     
-    public Enemie(String name,int strenght){
-        super(name, strenght);
+    public Enemie(String name,int strength){
+        super(name, strength);
     }
     
     public Equipement dropItem(){
